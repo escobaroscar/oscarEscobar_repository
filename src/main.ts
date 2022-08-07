12e511871b2e8dd2568be6a5eb1aee13503aaeca
Oscar Escobar <oescobar@grupo-link.com>
2022-08-07 15:20:27 -0500

    Fix: (Configuración): Configuración de variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import {  ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   //TODO:Add global validations 
@@ -11,6 +12,9 @@ async function bootstrap() {
     whitelist:true,
     forbidNonWhitelisted:true,
   })),
-  await app.listen(3000);
+   
+  await app.listen(process.env.PORT||3000);
+  console.log(`Server listening on port: ${await app.getUrl()}`);
+
 }
 bootstrap();
